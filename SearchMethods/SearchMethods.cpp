675805f0825de344333677bec094830d9b40eb05
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-10-13 23:01:58 +0100

    Removed tester, added execution duration timer, multiple runs

--- SearchMethods/SearchMethods.cpp
+++ SearchMethods/SearchMethods.cpp
@@ -1,25 +1,28 @@
 #include "BlockGrid.h"
-#include "BlockGridTester.h"
+#include <iostream>
+#include "DFS.h"
+#include "BFS.h"
+#include <chrono>
 
 int main()
-{
-	// ----- Copy ctor test -----
+{	
+	srand((unsigned int)time(NULL));
+	const BlockGrid grid(4, 4, { {3,0},{3,1},{3,2} }, { {1,1},{2,1},{3,1} }, { 3,3 });
 
-	BlockGrid grid(4, 4, { {3,0},{3,1},{3,2} }, { {1,1},{2,1},{3,1} }, { 3,3 });
-	{
-		BlockGrid grid2 = grid;
-		BlockGrid grid3(grid);
-
-		grid2.MoveAgent(Move::Left);
-		grid2.MoveAgent(Move::Left);
-		grid3.MoveAgent(Move::Up);
-		grid3.MoveAgent(Move::Up);
+	constexpr int runs = 100;
+	double sum = 0;
+	for (int i = 1; i < runs+1; i++) {
+		auto start = std::chrono::steady_clock::now();
+		//BFS::Search(grid);
+		DFS::Search(grid);
+		auto end = std::chrono::steady_clock::now();
+		std::chrono::duration<double, std::milli> duration = end - start;
+		double elapsed = duration.count();
+		std::cout << i << " | Time elapsed: " << elapsed << "ms" << std::endl;
+		sum += elapsed;
 	}
+	double avg = sum / runs;
+	std::cout << std::endl << "Average time elapsed: " << avg << "ms" << std::endl;
 
-	BlockGridTester tester(grid);
-	tester.Run();
-
-	// --------------------------
-
-    return 0;
+	return 0;
 }
