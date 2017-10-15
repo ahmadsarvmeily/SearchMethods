7245d0501b23f7e21ab9a344ff0bed403a3a70f3
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-10-15 21:45:32 +0100

    Minor optimisation

--- SearchMethods/DFS.h
+++ SearchMethods/DFS.h
@@ -13,18 +13,20 @@ public:
 		states.push({ root,0 });
 
 		if (limit > 0 || limit == -1) {
-			for (int depth = 0; !states.empty(); depth++) {
+			while (!states.empty()) {
 				Node currentState = states.top();
 				states.pop();
 
 				for (const Move& move : currentState.grid.GetMovesShuffled()) {
-					BlockGrid grid = currentState.grid;
-					if (grid.MoveAgent(move).IsInGoalState()) {
-						std::cout << currentState.depth + 1 << std::endl;
+					Node node = currentState;
+					node.depth++;
+					if (node.grid.MoveAgent(move).IsInGoalState()) {
+						std::cout << node.depth << std::endl;
 						return true;
 					}
-					if (currentState.depth < limit || limit == -1)
-						states.push({ grid,currentState.depth + 1 });
+					if (node.depth < limit || limit == -1) {
+						states.push(node);
+					}
 				}
 
 			}
